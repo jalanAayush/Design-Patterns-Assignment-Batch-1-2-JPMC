53a4f0f3c5fa28a45c7cc6cafcf2b6439f70c651
shawdipesh <71761216+shawdipesh@users.noreply.github.com>
2022-09-15 23:26:07 +0530

    Add files via upload

---
+++ Dipesh_Shaw_Design_Pattern_assignment/AbstractFactory_Assignment/ICarEngineAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory_Assignment
+{
+    public interface ICarEngineAction
+    {
+        public void checkCarEngineStatus();
+        public void addCarEngine();
+
+        public void removeCarEngine();
+
+        public void replaceCarEngine();
+
+
+    }
+}
---
+++ Dipesh_Shaw_Design_Pattern_assignment/BuilderPattern_Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace BuilderPattern_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Computer type \n1.Desktop \n2.Laptop \n3.Apple \n4.Exit");
+            IComputerFactory computer= null;
+
+            LaptopManufacturer computerShop=new LaptopManufacturer();
+
+            string computerType=Console.ReadLine();
+            if(computerType == "Desktop")
+            {
+                computer = new DesktopBuilder();
+
+            }
+            else if(computerType == "Laptop")
+            {
+                computer=new LaptopMaker();
+            }
+            else if(computerType=="Apple")
+            {
+                computer = new MacBookBuilder();
+            }
+            else
+            {
+                System.Environment.Exit(0);
+            }
+
+            computerShop.ConstructComputer(computer);
+
+
+        }
+    }
+}
