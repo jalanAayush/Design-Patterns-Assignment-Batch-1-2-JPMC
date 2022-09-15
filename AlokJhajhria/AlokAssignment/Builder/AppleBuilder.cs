076b910e3a0b013ffd65b9aa8d0d61bbc1b89f48
alokjhajhria <43287836+alokjhajhria@users.noreply.github.com>
2022-09-15 23:39:32 +0530

    Added Alok Assignment

---
+++ AlokJhajhria/AlokAssignment/Builder/AppleBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class AppleBuilder : ComputerBuilder
+    {
+        public AppleBuilder()
+        {
+            computer = new Computer("Apple");
+        }
+        public override void BuildHardDisk()
+        {
+            computer["hardDisk"] = "Apple Hard Disk";
+        }
+
+        public override void BuildMotherBoard()
+        {
+            computer["motherBoard"] = "Apple Mother Board";
+        }
+
+        public override void BuildProcessor()
+        {
+            computer["processor"] = "Apple Processor";
+        }
+
+        public override void BuildScreen()
+        {
+            computer["screen"] = "Apple Screen";
+        }
+    }
+}
---
+++ AlokJhajhria/AlokAssignment/Builder/DesktopBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class DesktopBuilder : ComputerBuilder
+    {
+        public DesktopBuilder()
+        {
+            computer = new Computer("Desktop");
+        }
+        public override void BuildHardDisk()
+        {
+            computer["hardDisk"] = "Desktop Hard Disk";
+        }
+
+        public override void BuildMotherBoard()
+        {
+            computer["motherBoard"] = "Desktop Mother Board";
+        }
+
+        public override void BuildProcessor()
+        {
+            computer["processor"] = "Desktop Processor";
+        }
+
+        public override void BuildScreen()
+        {
+            computer["screen"] = "Desktop Screen";
+        }
+    }
+}
