b771fe8730ced01d4ad2a787cc160258b5628498
malhotrakartik <43827556+malhotrakartik@users.noreply.github.com>
2022-09-16 12:35:31 +0530

    Add files via upload
    Assignments

---
+++ KartikMalhotra/BuilderPatternAssignment/AppleShop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuilderPatternAssignment.Builder;
+namespace BuilderPatternAssignment.Shop.Companies
+{
+    internal class AppleShop : ComputerShop
+    {
+        public AppleShop()
+        {
+            Console.WriteLine("Apple - Computers");
+        }
+        public override void Construct(ComputerBuilder computerbuilder)
+        {
+            computerbuilder.BuildHardDisk();
+            computerbuilder.BuildProcessor();
+            computerbuilder.BuildRam();
+            computerbuilder.BuildMotherboard();
+            computerbuilder.BuildScreen();
+        }
+    }
+}
