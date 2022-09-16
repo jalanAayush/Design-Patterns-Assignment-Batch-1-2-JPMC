e751fb19a9adbbd2eee16b3368020987fe7ccf39
jalanAayush <112081248+jalanAayush@users.noreply.github.com>
2022-09-16 08:23:54 +0530

    By Abhishek Joshi

---
+++ DesignPattern Assignments/AbstractFactoryAssignment/Revtron.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryAssignment
+{
+     public  class Revtron : ICarEngine
+    {
+
+        public override string showEngine()
+        {
+            return "THIS IS A REVTRON ENGINE";
+        }
+    }
+}
