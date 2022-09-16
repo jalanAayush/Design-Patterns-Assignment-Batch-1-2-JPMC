3bdcb2d83cd3f0f2d0f1a8f871f9c05ac4f26bf6
Aryan Gupta <67314254+aryangupta198@users.noreply.github.com>
2022-09-16 08:34:03 +0530

    Both assignments added

---
+++ Aryan_Gupta_Assignment/AbstractFactoryAssignment/Client.cs
@@ -0,0 +1,27 @@
+﻿using AbstractFactoryAssignment.Vehicles.VehicleFactory;
+using AbstractFactoryAssignment.Vehicles.VehicleFactory.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryAssignment.Vehicles
+{
+    internal class Client
+    {
+        private ICarEngine _carEngine;
+        private ICarLight _carLight;
+
+        public Client(IVehicleFactory vehicleFactory)
+        {
+            _carEngine = vehicleFactory.CreateCarEngine();
+            _carLight = vehicleFactory.CreateCarLight();
+        }
+
+        public void ShowCaseCarEngine()
+        {
+            _carEngine.AttachLight(_carLight);
+        }
+    }
+}
---
+++ Aryan_Gupta_Assignment/AbstractFactoryAssignment/ICarLight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryAssignment.Vehicles.VehicleFactory.Products
+{
+    internal interface ICarLight
+    {
+        public string Color();
+    }
+}
