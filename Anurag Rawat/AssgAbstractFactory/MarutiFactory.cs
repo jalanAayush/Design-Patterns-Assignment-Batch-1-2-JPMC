ca424d719f0105603c2def91e13f86d36d7fa28b
Anurag Rawat <68148139+anuragrawat015@users.noreply.github.com>
2022-09-16 08:34:50 +0530

    Add files via upload

---
+++ Anurag Rawat/AssgAbstractFactory/MarutiFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssgAbstractFactory
+{
+    internal class MarutiFactory : IVehicleFactory
+    {
+        public override ICarEngine CarEngine()
+        {
+            return new Revtron();
+        }
+
+        public override ICarLight CarLight()
+        {
+            return new Helogan();
+        }
+    }
+}
---
+++ Anurag Rawat/BuilderComputer/ComputerBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderComputer
+{
+    abstract class ComputerBuilder
+    {
+        protected Computer computer;
+
+        public Computer Computer
+        {
+            get { return computer; }
+        }
+
+        abstract public void BuildHardDisk();
+        abstract public void BuildMotherBoard();
+        abstract public void BuildProcessor();
+        abstract public void BuildScreen();
+
+    }
+}
