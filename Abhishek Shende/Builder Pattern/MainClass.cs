611a992d5a959ab8863ff409a26fe16d81cffb2a
abhi1712000 <57902696+abhi1712000@users.noreply.github.com>
2022-09-16 09:13:19 +0530

    Add files via upload

---
+++ Abhishek Shende/Builder Pattern/MainClass.cs
@@ -0,0 +1,42 @@
+﻿
+namespace BuilderPattern
+{
+    class MainClass
+    {
+
+        public static void Main()
+        {
+            ComputerShop? shop;
+            BuilderComputer? builder;
+            char cont='n';
+            int choice =1;
+            do
+            {
+                Console.Write("Press\n 1.Desktop\n 2.Mac \n 3.Laptop\n Enter your choice : ");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        builder = new BuilderDesktop();
+                        break;
+                    case 2:
+                        builder = new BuilderMac();
+                        break;
+                    case 3: 
+                        builder = new BuilderLaptop();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        return;
+                       
+                }
+                shop = new ComputerShop();
+                shop.BuildComputer(builder);
+                builder.Computer.DisplayInformation();
+                Console.Write("Do you want to continue ? y/N  : ");
+                cont = char.Parse(Console.ReadLine());
+            } while (cont == 'y' || cont == 'Y');
+        }
+    }
+}
